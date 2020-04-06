e8673d1e2151985020df70ec472a92c348e9bcfc
mgbachi_ugo <ugochukwu_mgbachi@yahoo.com>
2020-04-06 01:39:06 +0100

    button click feature added

--- app/src/main/java/com/mgbachi_ugo/hngshop/ProductActivity.kt
+++ app/src/main/java/com/mgbachi_ugo/hngshop/ProductActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_product.*
 import kotlinx.android.synthetic.main.product_item.*
 
@@ -23,7 +24,15 @@ class ProductActivity : AppCompatActivity() {
 
         val resourceId = resources.getIdentifier(info.image, "drawable", packageName)
         productview.setImageResource(resourceId)
-        
+
+        btn_buy.setOnClickListener {
+            Toast.makeText(this, "your item is being processed", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_home.setOnClickListener {
+            Toast.makeText(this, "you can have free drive and check it out \n its free!!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 
--- app/src/main/java/com/mgbachi_ugo/hngshop/UnitProduct.kt
+++
@@ -1,3 +0,0 @@
-package com.mgbachi_ugo.hngshop
-
-class UnitProduct (val title: String, val price: String, val  image: String)
